8915e9306752b82f4e9b31a250341c2a953d8a99
wunaiqiezixin <1947685764@qq.com>
2026-03-24 17:22:26 +0800

    solve collision: separate chaining

--- HashSep.c
+++ HashSep.c
@@ -9,7 +9,7 @@ FatalError(const char* message)
 static int
 NextPrime(int num)
 {
-    //todo: find an minmum prime 
+    //todo: find a minimum prime 
     return num;
 }
 
@@ -30,6 +30,19 @@ struct HashTbl
     int TableSize;
     List *TheLists;
 };
+
+/* Hash function */
+
+Index
+Hash(ElementType Key, HashTable H)
+{   
+    unsigned int HashVal = 0;
+    while (*Key)
+        HashVal = HashVal << 5 + *Key++;
+    
+    return HashVal % H->TableSize;
+}
+
 
 /* 分离链接法的初始化例程 */
 HashTable
@@ -68,4 +81,84 @@ InitializeTable(int TableSize)
     }
 
     return H;
+}
+
+Position
+Find(ElementType Key, HashTable H)
+{
+    Position P;
+    List L; /* dummy header */
+
+    L = H->TheLists[ Hash(Key, H->TableSize) ];
+    P = L->Next;
+    while (P != NULL && P->Element != Key)
+        P = P->Next;
+    return P;
+}
+
+
+void
+Insert(ElementType Key, HashTable H)
+{
+    Position Pos, NewCell;
+    List L;
+
+    Pos = Find(Key, H);
+    if (Pos == NULL) /* Not found */
+    {
+        NewCell = malloc(sizeof(struct ListNode));
+        if (NewCell == NULL)
+            FatalError("Out of space!!!");
+        else
+        {
+            L = H->TheLists[ Hash(Key, H->TableSize) ];
+            NewCell->Next = L->Next;
+            NewCell->Element = Key;
+            L->Next = NewCell;
+        }
+    }
+}
+
+void 
+Delete(ElementType Key, HashTable H)
+{
+    List L = H->TheLists[ Hash(Key, H->TableSize) ];
+
+    Position P = L;
+
+    while (P->Next && P->Next->Element != Key)
+        P = P->Next;
+    if (P == NULL)
+        return;
+    Position TmpNode = P->Next;
+    P->Next = P->Next->Next;
+    free(TmpNode);
+    return;
+}
+
+void 
+DestroyTable(HashTable H)
+{
+    int i;
+    Position P;
+    List L;
+
+    for (i = 0; i < H->TableSize; i++)
+    {
+        L = H->TheLists[i];
+        P = L->Next;
+
+        while (P)
+        {
+            Position Tmp = P;
+            P = P->Next;
+            free(Tmp);
+        }
+        free(L);/* 释放表头 */
+    }
+
+    
+    free(L);/* 释放指针数组 */
+
+    free(H);
 }
--- HashSep.h
+++ HashSep.h
@@ -3,19 +3,20 @@
 #define MinTableSize (10)
 #include<stdlib.h>
 #include<stdio.h>/* NULL */
-
-typedef int ElementType;
+typedef unsigned int Index;
+typedef const char* ElementType;
 struct ListNode;
 typedef struct ListNode* Position;
 struct HashTbl;
 typedef struct HashTbl* HashTable;
 
+Index Hash(ElementType Key, HashTable H);
 HashTable InitializeTable(int TableSize);
 void DestroyTable(HashTable H);
 Position Find(ElementType Key, HashTable H);
 void Insert(ElementType Key, HashTable H);
 ElementType Retrieve(Position P);
-void Dlete(ElementType Key, HashTable H);
+void Delete(ElementType Key, HashTable H);
 void MakeEmpty(HashTable H);
 
 #endif /* _HashSep_H */
